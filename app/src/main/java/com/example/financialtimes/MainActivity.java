a9441f3573aff091801e0917645ca37d512574fa
Razvan Papurica <razvan.papurica@stud.etti.upb.ro>
2019-03-25 00:38:42 +0200

    --allow-empty-message

--- app/src/main/java/com/example/financialtimes/MainActivity.java
+++ app/src/main/java/com/example/financialtimes/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.financialtimes;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -33,12 +35,21 @@ public class MainActivity extends AppCompatActivity {
         return true;
         }
     };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Fragment defaultFragment = new cursFragment();
+
         BottomNavigationView bottom_nav = findViewById(R.id.bottom_nav);
         bottom_nav.setOnNavigationItemSelectedListener(nav_handler);
+
+        if(savedInstanceState == null){
+            FragmentManager frag_handler = getSupportFragmentManager();
+            FragmentTransaction defaultFragTransaction = frag_handler.beginTransaction();
+            defaultFragTransaction.replace(R.id.frag_container, defaultFragment).commit();
+        }
     }
 }
--- app/src/main/java/com/example/financialtimes/cursFragment.java
+++ app/src/main/java/com/example/financialtimes/cursFragment.java
@@ -1,18 +1,84 @@
 package com.example.financialtimes;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.w3c.dom.Text;
+
+import java.net.HttpURLConnection;
+import java.io.IOException;
 
 public class cursFragment extends Fragment {
+
+    private double[] curs_valori = new double[30];
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.curs_fragment, container, false);
+
+        final View view = inflater.inflate(R.layout.curs_fragment, container, false);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+
+            public void run() {
+                int i = 0;
+                try{
+                    Document doc = Jsoup.connect("https://www.cursbnr.ro/curs-valutar-bnr").get();
+                    Elements data_curs = doc.select("table tbody tr td:first-child");
+                    Elements pret_curs = doc.select("table tbody tr td:nth-child(2)");
+
+                    for (Element pret: pret_curs){
+                        curs_valori[i] = Double.parseDouble(pret.text());
+                        i++;
+                    }
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                LineGraphSeries<DataPoint> series1 = new LineGraphSeries<>(fetchVal());
+                GraphView graph1 = view.findViewById(R.id.graph1);
+
+                graph1.addSeries(series1);
+                graph1.getViewport().setScrollable(true);
+                graph1.getViewport().setScalable(true);
+                graph1.getViewport().setMinY(4.6);
+                graph1.getViewport().setMaxY(4.9);
+
+                GridLabelRenderer glr = graph1.getGridLabelRenderer();
+                glr.setPadding(36);
+            }
+        });
+
+        thread.start();
+
+        return view;
+    }
+
+    public DataPoint[] fetchVal(){
+        DataPoint[] values = new DataPoint[curs_valori.length];
+
+        for (int i = 0; i < curs_valori.length; i++){
+            DataPoint temp = new DataPoint(i, curs_valori[i]);
+            values[i] = temp;
+        }
+        return values;
     }
 }
